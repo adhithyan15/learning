1f0d8b3d250fd4b5fd7f98a6ec39ccf8d78ddaf8
Adhithya Rajasekaran <adhithyan15@gmail.com>
2022-12-06 00:46:10 -0800

    Implement Array Stack

--- code/packages/ruby/array_stack_ds/lib/array_stack_ds.rb
+++ code/packages/ruby/array_stack_ds/lib/array_stack_ds.rb
@@ -3,6 +3,53 @@
 require_relative "array_stack_ds/version"
 
 module ArrayStackDS
-  class Error < StandardError; end
-  # Your code goes here...
+  class Stack
+    def initialize(max_capacity = nil)
+      @stack_elements = []
+      @max_capacity = max_capacity
+    end
+
+    def push(element_to_add)
+      unless @max_capacity.nil?
+        if @stack_elements.length >= @max_capacity
+          raise StackOverflowError.new
+        end
+      end
+      @stack_elements.unshift(element_to_add)
+    end
+
+    def pop
+      if @stack_elements.length == 0
+        raise StackUnderflowError.new
+      end
+      return @stack_elements.shift
+    end
+
+    def peek
+      if @stack_elements.length == 0
+        raise StackUnderflowError.new
+      end
+      return @stack_elements[0]
+    end
+
+    def top
+      return peek()
+    end
+
+    def length
+      return @stack_elements.length
+    end
+  end
+
+  class StackOverflowError < StandardError
+    def message
+      return "Stack has overflowed"
+    end
+  end
+
+  class StackUnderflowError < StandardError
+    def message
+      return "Stack has underflowed"
+    end
+  end
 end
